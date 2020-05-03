3a94d7eb821a2dc0d108324ff627e9aca8921678
Ravishankarcode <54878415+Ravishankarcode@users.noreply.github.com>
2020-05-03 12:57:50 +0530

    file modifies

--- property-event-binding/src/app/app.component.ts
+++ property-event-binding/src/app/app.component.ts
@@ -9,8 +9,13 @@ export class AppComponent {
   name = 'Angular';
   buttonDisabled = false;
   serverCreationStatus = "No Server was created";
+  serverName = "something";
 
   onCreateServer(){
     this.serverCreationStatus ="Server is created";
+  }
+
+  onUpdateServerName(event : any){
+   this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
